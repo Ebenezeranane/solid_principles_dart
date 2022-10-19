a67126d92fa419318b4929a228b74765f8aaf1de
= <=>
2022-10-19 05:50:31 -0400

    first commit

---
+++ bin/solid_principles_dart.dart
@@ -0,0 +1,5 @@
+import 'package:solid_principles_dart/solid_principles_dart.dart' as solid_principles_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${solid_principles_dart.calculate()}!');
+}
